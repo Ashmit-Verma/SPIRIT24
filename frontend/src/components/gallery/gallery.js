46b547803f1361424a38b7d05a161a52820e35fe
Ashmit Verma <a1verma8105@gmail.com>
2024-05-31 13:13:31 +0530

    gallery sorted

--- frontend/src/components/gallery/gallery.js
+++ frontend/src/components/gallery/gallery.js
@@ -1,5 +1,5 @@
 // src/Gallery.js
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './gallery.css';
 
 const Gallery = () => {
@@ -9,16 +9,63 @@ const Gallery = () => {
     "img_3.jpg",
     "img_4.jpg",
     "img_5.jpg",
+    "img_6.jpg",
+    "img_7.jpg",
+    "img_1.jpg",
+    "img_2.jpg",
   ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
+  const [isTransitioning, setIsTransitioning] = useState(false);
+  const totalImages = images.length;
+  const carouselRef = useRef(null);
+
+  useEffect(() => {
+    if (!isHovered) {
+      const interval = setInterval(() => {
+        setIsTransitioning(true);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      }, 2000);
+
+      return () => clearInterval(interval);
+    }
+  }, [isHovered]);
+
+  useEffect(() => {
+    if (currentIndex === totalImages) {
+      setTimeout(() => {
+        setIsTransitioning(false);
+        setCurrentIndex(0);
+      }, 500); // Duration of the transition
+    }
+  }, [currentIndex, totalImages]);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <div className="gallery-container">
       <h1>Gallery</h1>
-      <div className="gallery">
-        {images.map((image, index) => (
-          <div className="card" key={index}>
-            <img className="galleryimg" src={image} alt={`Image ${index}`} />
-          </div>
-        ))}
+      <div className="carousel" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div
+          className={`carousel-track ${isTransitioning ? 'transition' : ''}`}
+          ref={carouselRef}
+          style={{
+            transform: `translateX(-${(currentIndex % totalImages) * (100 / totalImages)}%)`,
+          }}
+        >
+          {images.concat(images).map((image, index) => (
+            <div className="card" key={index}>
+              <img className="galleryimg" src={image} alt={`Image ${index}`} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
