30e8df8d64e441a1d41b2ede4fdb3289d70e5107
Ashmit Verma <a1verma8105@gmail.com>
2024-06-14 22:57:16 +0530

    commit changes

--- backend/controllers/loginControllers.js
+++ backend/controllers/loginControllers.js
@@ -17,8 +17,8 @@ const sendConfirmationEmail = async (email) => {
   const mailOptions = {
     from: process.env.EMAIL,
     to: email,
-    subject: 'Login Confirmation',
-    text: 'Thank you for logging in for the first time!',
+    subject: 'SPIRIT 2024- Campus Ambassador Registration Successful!',
+    text: "Congratulations! We have received your registration for the Campus Ambassador program for SPIRIT, the annual sports festival of IIT Guwahati. We're thrilled to have you on board and look forward to working closely with you to promote our festival ,brand and engage with your campus community. As a Campus Ambassador, you'll play a vital role in representing SPIRIT on your campus, organizing events, and spreading the word about our mission and initiatives.Stay tuned for further communication regarding upcoming events, training sessions,tasks and exclusive opportunities reserved for you as ambassadors for SPIRIT.Once again, welcome to the team , and thank you for joining us on this exciting journey!Best regards, TEAM SPIRIT.",
   };
 
   try {
