f7b0b9aa718d685b2a1c298d081536c30210a44e
Ashmit Verma <a1verma8105@gmail.com>
2024-06-18 21:14:28 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 
 app.use(express.json());
 const corsOptions = {
-  origin: 'https://6671a8ba803efa009fdbf460--timely-mochi-34f86a.netlify.app/',
+  origin: 'https://6671a8ba803efa009fdbf460--timely-mochi-34f86a.netlify.app',
   optionsSuccessStatus: 200,
 };
 
