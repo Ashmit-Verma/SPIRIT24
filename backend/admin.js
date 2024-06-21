426da1ff4eee481b1def2cdc22376998e362ae2a
Ashmit Verma <a1verma8105@gmail.com>
2024-06-21 09:10:16 +0530

    adminjs changes

--- backend/admin.js
+++ backend/admin.js
@@ -3,7 +3,7 @@ import AdminJSExpress from '@adminjs/express';
 import AdminJSSequelize from '@adminjs/sequelize';
 import sequelize from './config/database.js';
 import User from './models/User.js';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 import argon2 from 'argon2'; // Import argon2 library
 
 dotenv.config();
@@ -20,7 +20,7 @@ const adminOptions = {
       options: {
         properties: {
           password: {
-            isVisible: { list: false, filter: false, show: false, edit: true },
+            isVisible: { list: false, filter: false, show: true, edit: true },
           },
           encryptedPassword: {
             type: 'string',
@@ -35,6 +35,7 @@ const adminOptions = {
         },
         actions: {
           new: {
+            isVisible: true,
             before: async (request) => {
               console.log('Creating new user:', request.payload);
               if (request.payload.password) {
@@ -52,7 +53,7 @@ const adminOptions = {
               if (request.payload.password) {
                 const hashedPassword = await argon2.hash(request.payload.password); // Hash password using argon2
                 request.payload.encryptedPassword = hashedPassword;
-                request.payload.password = undefined;
+                // request.payload.password = undefined;
               }
               console.log('Processed edit user:', request.payload);
               return request;
@@ -66,15 +67,22 @@ const adminOptions = {
 
 const adminJs = new AdminJS(adminOptions);
 
+const sessionOptions = {
+  resave: false, // Don't save session if unmodified
+  saveUninitialized: false, // Don't create session until something stored
+  secret: process.env.COOKIE_SECRET,
+  cookie: { secure: process.env.NODE_ENV === 'production' },
+};
+
 const router = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
   authenticate: async (email, password) => {
     if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
-      console.log("Correct");
+      console.log('Correct');
       return { email: process.env.ADMIN_EMAIL };
     }
     return null;
   },
   cookiePassword: process.env.COOKIE_SECRET,
-});
+}, null, sessionOptions);
 
 export { adminJs, router };
